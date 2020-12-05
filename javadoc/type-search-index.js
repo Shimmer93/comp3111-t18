7df30e03cb75709728d83cd01d1a7fc731c03cad
Nilbug <70528707+Nilbug@users.noreply.github.com>
2020-12-05 12:12:36 +0800

    Upload JavaDoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"comp3111.popnames","l":"AnalyzeNames"},{"p":"comp3111.popnames","l":"Controller"},{"p":"comp3111.popnames","l":"MyApplication"},{"p":"comp3111.popnames","l":"TableWrapper"}]
